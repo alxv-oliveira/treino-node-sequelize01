ef806f0512ebea1bc1c1eb1054d18f0310a18e5a
Wenderson <wps713@gmail.com>
2020-04-02 18:48:53 -0300

    Linting routes file

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,21 @@ const moment = require('moment');
 const AttendanceController = require('./controllers/AttendanceController');
 
 function dataValida(req, res, next) {
-    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
-    const data = moment(req.body.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-    
-    const dataEhValida = data >= dataCriacao;
+  const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
 
-    if(!dataEhValida)
-        return res.status(400).json({ error: 'Data deve ser maior ou igual a data atual'});
+  const data = moment(req.body.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
 
-    return next();
+  const dataEhValida = data >= dataCriacao;
+
+  if (!dataEhValida) { return res.status(400).json({ error: 'Data deve ser maior ou igual a data atual' }); }
+
+  return next();
 }
 
-module.exports = routes => {
-    routes.get('/attendances', AttendanceController.index)
-        .get('/attendance', AttendanceController.show)
-        .post('/attendances', dataValida, AttendanceController.store)
-        .patch('/attendances/:id', AttendanceController.update)
-        .delete('/attendances/:id', AttendanceController.delete);
+module.exports = (routes) => {
+  routes.get('/attendances', AttendanceController.index)
+    .get('/attendance', AttendanceController.show)
+    .post('/attendances', dataValida, AttendanceController.store)
+    .patch('/attendances/:id', AttendanceController.update)
+    .delete('/attendances/:id', AttendanceController.delete);
 };
